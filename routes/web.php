c69cde4d59886e55ecd91d269aa06270fe41a797
Madgen Del Castillo <madgen.delcastillo@gmail.com>
2026-07-02 14:25:32 -0500

    Eliminacion Tests

--- routes/web.php
+++ routes/web.php
@@ -5,23 +5,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/test', function () {
-    return view('page_test');
-});
-
-Route::get('/test/clone', function () {
-    return view('page_test_clone');
-});
-
-Route::get('/ui/test', function () {
-    return view('ui.test');
-});
-
-Route::get('/layout/test', function () {
-    return view('layout.test');
-});
-
-Route::get('/forms/test', function () {
-    return view('forms.test');
-});
